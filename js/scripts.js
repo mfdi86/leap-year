a625d2264b0b913e5befa466c79cd18c6dfff0c9
Dylan Dills and Maria Del Castillo <maria@>
2017-05-02 16:27:21 -0700

    show if the result variable is false, the text with read not a leap year, and if the result it true, it will read, it is a leap year

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 ===0)) {
     return true;
   } else {
     return false;
@@ -13,6 +13,15 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
+
+    $(".year").text("year");
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
     $("#result").text(result);
   });
 });
